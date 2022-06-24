0d6cc53bed0df8c2aacc4cde7d408f4ea6928bb4
Aldo Zulhaq <44134685+aldozulhaq@users.noreply.github.com>
2022-06-24 19:32:54 +0700

    dialog

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -13,6 +13,8 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text dialogueText;
     public GameObject dialogueBox;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
