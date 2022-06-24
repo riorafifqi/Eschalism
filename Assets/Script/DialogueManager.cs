0616919770c4bb5d0337ddd7c1255509d5ef4ba5
Aldo Zulhaq <44134685+aldozulhaq@users.noreply.github.com>
2022-06-24 19:33:11 +0700

    Revert "dialog"
    This reverts commit 0d6cc53bed0df8c2aacc4cde7d408f4ea6928bb4.

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -13,8 +13,6 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text dialogueText;
     public GameObject dialogueBox;
 
-
-
     // Start is called before the first frame update
     void Start()
     {
